a851bbe8e0864f0240c3a12d6ee8edecb6c0b134
Blake Huber <blakeca00@gmail.com>
2018-05-05 05:57:12 -0500

    core/perlconfig.sh: implement QUIET to suppress stdout when scripted caller

--- rkhunter/core/perlconfig.sh
+++ rkhunter/core/perlconfig.sh
@@ -2,34 +2,32 @@
 
 #_________________________________________________________________________
 #                                                                         |
-#                                                                         |
 #  Author:   Blake Huber                                                  |
 #  Purpose:  # Perl Module Updates | rkhunter                             |
-#  Location: $EC2_REPO                                                    |
 #  Requires: rkhunter, prom                                               |
 #  Environment Variables (required, global):                              |
 #  User:     $user                                                        |
 #  Output:   CLI                                                          |
 #  Error:    stderr                                                       |
 #  Log:  $pkg_path/logs/prom.log                                          |
-#                                                                         |
-#                                                                         |
 #_________________________________________________________________________|
 
 
 #  Verify DISTRO; install Develpment tools (AML) || build essentials, etc (installs make)
 #  Install cpan if not present using distro-specific pkg mgr
+#  Update cpan
 #  Run perl script to configure cpan if not configured previously.  (possible solution, may want to install cpanm per the link (see below)
 
 
 # globals
 pkg=$(basename $0)                                      # pkg (script) full name
-pkg_root="$(echo $pkg | awk -F '.' '{print $1}')"         # pkg without file extention
+pkg_root="$(echo $pkg | awk -F '.' '{print $1}')"       # pkg without file extention
 pkg_path=$(cd $(dirname $0); pwd -P)                    # location of pkg
 host=$(hostname)
 system=$(uname)
 TMPDIR='/tmp'
-perlconf_version='1.0'
+perlconf_version='1.2'
+QUIET="$1"                                              # Supress output to stdout; from caller
 
 # arrays
 declare -a ARR_MODULES
@@ -124,7 +122,7 @@ function root_permissions(){
 
 
 root_permissions
-dep_check $LOG_DIR $LOG_FILE
+depcheck $LOG_DIR $LOG_FILE
 
 # ----- begin ----- #
 
@@ -136,22 +134,31 @@ std_message "Generating list of missing ${yellow}Perl${reset} Modules. Scan Test
 \t      without these; however, Adding them will increase accuracy
 \t      of malware scanning tests performed by Rkhunter." "INFO"
 
-echo -e "\n${title}Rkhunter${bodytext} ${yellow}Perl${reset} Module Dependency Status\n" | indent04
-sudo $RK --list perl 2>/dev/null  | tail -n +3 | tee /dev/tty | grep MISSING | awk '{print $1}' > $TMPDIR/perl_pkg.list
+if [ $QUIET ]; then
+    sudo $RK --list perl 2>/dev/null  | tail -n +3 | grep MISSING | awk '{print $1}' > $TMPDIR/perl_pkg.list
+    std_logger "Missing perl modules list:\n $(cat $TMPDIR/perl_pkg.list)" "INFO" $LOG_FILE
+else
+    echo -e "\n${title}Rkhunter${bodytext} ${yellow}Perl${reset} Module Dependency Status\n" | indent04
+    sudo $RK --list perl 2>/dev/null  | tail -n +3 | tee /dev/tty | grep MISSING | awk '{print $1}' > $TMPDIR/perl_pkg.list
+fi
 
 num_modules=$(cat $TMPDIR/perl_pkg.list | wc -l)
 
 if [ "$num_modules" = "0" ]; then
-    std_message "All perl module dependencies are installed." "INFO" $LOG_FILE
+    std_message "All module dependencies are installed." "INFO" $LOG_FILE
 else
-    std_message "There $num_modules that can be installed on your machine to complete the Rkhunter setup." "INFO"
-    echo -e "\n"
-    read -p "     Do you want to continue?  [y]:" CHOICE
-    if [ -z $CHOICE ] || [ "$CHOICE" = "y" ]; then
-        std_message "Begin Perl Module Update... " "INFO" $LOG_FILE
+    if [ $QUIET ]; then
+        std_logger "Skipping user prompt, quiet set (QUIET = $QUIET)" "INFO" $LOG_FILE
     else
-        std_message "Cancelled by user" "INFO" $LOG_FILE
-        exit 1
+        std_message "There perl $num_modules required by Rkhunter that can be installed on your machine" "INFO"
+        echo -e "\n"
+        read -p "     Do you want to continue?  [y]:" CHOICE
+        if [ -z $CHOICE ] || [ "$CHOICE" = "y" ]; then
+            std_message "Begin Perl Module Update... " "INFO" $LOG_FILE
+        else
+            std_message "Cancelled by user" "INFO" $LOG_FILE
+            exit 1
+        fi
     fi
     # build array of all missing modules
     ARR_MODULES=$(cat $TMPDIR/perl_pkg.list)
@@ -159,12 +166,24 @@ else
 
     for module in ${ARR_MODULES[@]}; do
         std_message "Installing perl module $module" "INFO" $LOG_FILE
-        $SUDO $cpan_bin -i $module
+        if [ $QUET ]; then
+            $SUDO $cpan_bin -i $module > /dev/null
+            std_logger "cpan installation msgs supressed" "INFO" $LOG_FILE
+        else
+            $SUDO $cpan_bin -i $module
+        fi
     done
 
-    echo -e "\n${title}Rkhunter${bodytext} Perl Module Dependency Status\n" | indent10
-    # print perl module report
-    $SUDO $RK --list perl 2>/dev/null | tail -n +3
+    if [ $QUET ]; then
+        echo -e "Rkhunter Perl Module Dependency Status" >> $LOG_FILE
+        $SUDO $RK --list perl 2>/dev/null | tail -n +3 >> $LOG_FILE
+    else
+        # print perl module report
+        echo -e "\n${title}Rkhunter${bodytext} Perl Module Dependency Status\n" | indent10
+        $SUDO $RK --list perl 2>/dev/null | tail -n +3
+    fi
 fi
+
+# perl configuration status
 std_message "Perl Module Config for Rkhunter ${green}COMPLETE${bodytext}" "INFO" $LOG_FILE
 exit 0
