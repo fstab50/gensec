c08cd78e8c0aab16ce54385f64c6b865a49d8718
Blake Huber <blakeca00@gmail.com>
2018-05-05 12:55:11 -0500

    rename, add skdet

---
+++ rkhunter/core/configure_skdet.sh
@@ -0,0 +1,189 @@
+#!/usr/bin/env bash
+
+#_________________________________________________________________________
+#                                                                         |
+#  Author:   Blake Huber                                                  |
+#  Purpose:  # Perl Module Updates | rkhunter                             |
+#  Requires: rkhunter, prom                                               |
+#  Environment Variables (required, global):                              |
+#  User:     $user                                                        |
+#  Output:   CLI                                                          |
+#  Error:    stderr                                                       |
+#  Log:  $pkg_path/logs/prom.log                                          |
+#_________________________________________________________________________|
+
+
+#  Verify DISTRO; install Develpment tools (AML) || build essentials, etc (installs make)
+#  Install cpan if not present using distro-specific pkg mgr
+#  Update cpan
+#  Run perl script to configure cpan if not configured previously.  (possible solution, may want to install cpanm per the link (see below)
+
+
+# globals
+pkg=$(basename $0)                                      # pkg (script) full name
+pkg_root="$(echo $pkg | awk -F '.' '{print $1}')"       # pkg without file extention
+pkg_path=$(cd $(dirname $0); pwd -P)                    # location of pkg
+host=$(hostname)
+system=$(uname)
+TMPDIR='/tmp'
+perlconf_version='1.2'
+QUIET="$1"                                              # Supress output to stdout; from caller
+
+# arrays
+declare -a ARR_MODULES
+
+# logging
+LOG_DIR="$HOME/logs"
+if [ ! $LOG_FILE ]; then LOG_FILE="$LOG_DIR/$pkg_root.log"; fi
+
+# source dependencies
+if [ $(echo $pkg_path | grep core) ]; then
+    # called standalone
+    source $pkg_path/colors.sh
+    source $pkg_path/exitcodes.sh
+    source $pkg_path/std_functions.sh
+else
+    # called by another script
+    source $pkg_path/core/colors.sh
+    source $pkg_path/core/exitcodes.sh
+    source $pkg_path/core/std_functions.sh
+fi
+
+
+# ---  declarations  -------------------------------------------------------------------------------
+
+
+function binary_depcheck(){
+    ## validate binary dependencies installed
+    local check_list=( "$@" )
+    local msg
+    #
+    for prog in "${check_list[@]}"; do
+        if ! type "$prog" > /dev/null 2>&1; then
+            msg="${title}$prog${bodytext} is required and not found in the PATH. Aborting (code $E_DEPENDENCY)"
+            std_error_exit "$msg" $E_DEPENDENCY
+        fi
+    done
+    #
+    # <<-- end function binary_depcheck -->>
+}
+
+function depcheck(){
+    ## validate cis report dependencies ##
+    local log_dir="$1"
+    local log_file="$2"
+    local msg
+    #
+    ## test default shell ##
+    if [ ! -n "$BASH" ]; then
+        # shell other than bash
+        msg="Default shell appears to be something other than bash. Please rerun with bash. Aborting (code $E_BADSHELL)"
+        std_error_exit "$msg" $E_BADSHELL
+    fi
+    ## logging prerequisites  ##
+    if [[ ! -d "$log_dir" ]]; then
+        if ! mkdir -p "$log_dir"; then
+            std_error_exit "$pkg: failed to make log directory: $log_dir. Exit" $E_DEPENDENCY
+        fi
+    fi
+    if [ ! -f $log_file ]; then
+        if ! touch $log_file 2>/dev/null; then
+            std_error_exit "$pkg: failed to seed log file: $log_file. Exit" $E_DEPENDENCY
+        fi
+    fi
+    ## check for required cli tools ##
+    binary_depcheck cpan perl rkhunter
+    # success
+    std_logger "$pkg: dependency check satisfied." "INFO" $log_file
+    #
+    # <<-- end function depcheck -->>
+}
+
+function root_permissions(){
+    ## validates required root privileges ##
+    if [ $EUID -ne 0 ]; then
+        std_message "You must run this installer as root or access root privileges via sudo. Exit" "WARN"
+        read -p "    Continue? [quit]: " CHOICE
+        if [ -z $CHOICE ] || [ "$CHOICE" = "quit" ] || [ "$CHOICE" = "q" ]; then
+            std_message "Re-run as root or execute with sudo:
+            \n\t\t$ sudo sh $pkg" "INFO"
+            exit 0
+        else
+            SUDO="sudo"
+        fi
+    else
+        SUDO=''
+    fi
+    return 0
+}
+
+
+# --- main ----------------------------------------------------------------------------------------
+
+
+root_permissions
+depcheck $LOG_DIR $LOG_FILE
+
+# ----- begin ----- #
+
+cd $TMPDIR
+RK=$($SUDO which rkhunter)
+
+# generate list of missing packages:
+std_message "Generating list of missing ${yellow}Perl${reset} Modules. Scan Tests will run
+\t      without these; however, Adding them will increase accuracy
+\t      of malware scanning tests performed by Rkhunter." "INFO"
+
+if [ $QUIET ]; then
+    sudo $RK --list perl 2>/dev/null  | tail -n +3 | grep MISSING | awk '{print $1}' > $TMPDIR/perl_pkg.list
+    std_logger "Missing perl modules list:\n $(cat $TMPDIR/perl_pkg.list)" "INFO" $LOG_FILE
+else
+    echo -e "\n${title}Rkhunter${bodytext} ${yellow}Perl${reset} Module Dependency Status\n" | indent04
+    sudo $RK --list perl 2>/dev/null  | tail -n +3 | tee /dev/tty | grep MISSING | awk '{print $1}' > $TMPDIR/perl_pkg.list
+fi
+
+num_modules=$(cat $TMPDIR/perl_pkg.list | wc -l)
+
+if [ "$num_modules" = "0" ]; then
+    std_message "All module dependencies are installed." "INFO" $LOG_FILE
+else
+    if [ $QUIET ]; then
+        std_logger "Skipping user prompt, quiet set (QUIET = $QUIET)" "INFO" $LOG_FILE
+    else
+        std_message "There perl $num_modules required by Rkhunter that can be installed on your machine" "INFO"
+        echo -e "\n"
+        read -p "     Do you want to continue?  [y]:" CHOICE
+        if [ -z $CHOICE ] || [ "$CHOICE" = "y" ]; then
+            std_message "Begin Perl Module Update... " "INFO" $LOG_FILE
+        else
+            std_message "Cancelled by user" "INFO" $LOG_FILE
+            exit 1
+        fi
+    fi
+    # build array of all missing modules
+    ARR_MODULES=$(cat $TMPDIR/perl_pkg.list)
+    cpan_bin=$(which cpan)
+
+    for module in ${ARR_MODULES[@]}; do
+        std_message "Installing perl module $module" "INFO" $LOG_FILE
+        if [ $QUET ]; then
+            $SUDO $cpan_bin -i $module > /dev/null
+            std_logger "cpan installation msgs supressed" "INFO" $LOG_FILE
+        else
+            $SUDO $cpan_bin -i $module
+        fi
+    done
+
+    if [ $QUET ]; then
+        echo -e "Rkhunter Perl Module Dependency Status" >> $LOG_FILE
+        $SUDO $RK --list perl 2>/dev/null | tail -n +3 >> $LOG_FILE
+    else
+        # print perl module report
+        echo -e "\n${title}Rkhunter${bodytext} Perl Module Dependency Status\n" | indent10
+        $SUDO $RK --list perl 2>/dev/null | tail -n +3
+    fi
+fi
+
+# perl configuration status
+std_message "Perl Module Config for Rkhunter ${green}COMPLETE${bodytext}" "INFO" $LOG_FILE
+exit 0
