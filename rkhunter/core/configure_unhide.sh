92a4aa4298e848291ea33528d0d9768baa6b2964
blakeca00 <blakeca00@gmail.com>
2018-05-12 06:07:07 -0500

    core/configure_unhide.sh: update to is_installed

--- rkhunter/core/configure_unhide.sh
+++ rkhunter/core/configure_unhide.sh
@@ -100,7 +100,7 @@ function depcheck(){
 function is_installed(){
     ## validate if binary previously installed  ##
     local binary="$1"
-    local location=$(which $binary)
+    local location=$(which $binary 2>/dev/null)
     if [ $location ]; then
         std_message "$binary is already compiled and installed:  $location" "INFO" $LOG_FILE
         return 0
